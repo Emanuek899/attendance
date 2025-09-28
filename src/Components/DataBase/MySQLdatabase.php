c7c7765ac2fe87e6437baa632d794c9b303bc205
Ralsei <emanuel.carbajalsantacruz@gmail.com>
2025-09-27 21:07:11 -0600

    <Implementation> DB component & interface

--- src/Components/DataBase/MySQLdatabase.php
+++ src/Components/DataBase/MySQLdatabase.php
@@ -12,17 +12,26 @@ class MySQLdatabase implements Database{
      * @param
      * @return 
     */
-    public function ejecutar(string $sql, array $params = []) {
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute($params);
+    public function ejecutar(string $sql, array $params = []): void {
+        try{
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+        }catch(PDOException $e){
+            throw $e;
+        }
+
     }
 
     /**
      * 
     */
-    public function consultar(string $sql, array $params = []) {
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute($params);
-        return $stmt->fetchall(PDO::FETCH_ASSOC);
+    public function consultar(string $sql, array $params = []): array {
+        try{
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->fetchall(PDO::FETCH_ASSOC);
+        }catch(PDOException $e){
+            throw $e;
+        }
     }
 }
--- src/Core/interfaces/Database.php
+++ src/Core/interfaces/Database.php
@@ -1,5 +1,5 @@
 <?php
 interface Database {
-    public function ejecutar(string $sql, array $params);
-    public function consultar(string $sql, array $params);
+    public function ejecutar(string $sql, array $params): void;
+    public function consultar(string $sql, array $params): array;
 }
