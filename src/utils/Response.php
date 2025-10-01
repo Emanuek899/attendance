e4cffe814cc62d1248af0b96990b04e6f8dfd8dd
Ralsei <emanuel.carbajalsantacruz@gmail.com>
2025-09-30 20:15:28 -0600

    utils folder

---
+++ src/utils/Response.php
@@ -0,0 +1,13 @@
+<?php
+class Response{
+    public static function response(array $data, int $code): void{
+        $status = ($code >= 200 && $code < 300) ? 'success' : 'error';
+        header('Content-Type: application/json');
+        http_response_code($code);
+        echo json_encode([
+            'status' => $status,
+            'data' => $data
+        ]);
+        exit;
+    }
+}
