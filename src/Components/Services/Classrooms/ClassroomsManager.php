fb6ef9fdd64372b3912881079b5de8dcd24dea94
Ralsei <emanuel.carbajalsantacruz@gmail.com>
2025-10-25 22:29:40 -0600

    edition in parameters of method, table added

--- src/Components/Services/Classrooms/ClassroomsManager.php
+++ src/Components/Services/Classrooms/ClassroomsManager.php
@@ -3,25 +3,39 @@ require_once __DIR__ . '/../BaseManager.php';
 require_once __DIR__ . '/../../../utils/validator.php';
 class ClassroomsManager extends BaseManager{
     private Validator $val;
+    const TABLE = 'classrooms';
 
-    public function __construct(ClassroomsRepository $repo, Validator $val){
+    public function __construct(MySQLRepositorie $repo, Validator $val){
         parent::__construct($repo);
         $this->val = $val;
     }
     
     public function create(array $data): array{
-        return parent::create($data);
+        return parent::createQuery(self::TABLE, $data);
     }
 
     public function read(array $conditions = [], array $cols = ['*']): array{
-        return parent::read($conditions, $cols);
+        return parent::readQuery(self::TABLE, $conditions, $cols);
     }
 
     public function update(array $data, array $conditions): array{
-        return parent::update($data, $conditions);
+        return parent::updateQuery(self::TABLE, $data, $conditions);
     }
 
     public function delete(array $conditions = []): array{
-        return parent::delete($conditions);
+        return parent::deleteQuery(self::TABLE, $conditions);
+    }
+
+    public function findById(int $id){
+        $conditions =[
+            [
+                'column' => 'classroom_id',
+                'op' => '=',
+                'val' => $id,
+                'boolean' => 'AND',
+            ]
+        ];
+        $cols = ['classroom_id'];
+        return parent::readQuery(self::TABLE, $conditions, $cols);
     }
 }
