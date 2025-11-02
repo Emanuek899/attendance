da6be207cff8c89f9a246942db8d59ea54120452
Ralsei <emanuel.carbajalsantacruz@gmail.com>
2025-11-02 00:17:23 -0600

    <Implementation> namespaces

--- src/Components/Services/BaseManager.php
+++ src/Components/Services/BaseManager.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . '/../../utils/status.php';
+namespace Components\Services;
+use Components\Repositories\MySQLRepositorie;
+// require_once __DIR__ . '/../../utils/status.php';
 
 class BaseManager {
 private object $repo;
