1ed940901e9bbda1c580e85ff79eca2feb392dcb
Ralsei <emanuel.carbajalsantacruz@gmail.com>
2025-11-02 00:23:26 -0600

    <Implementation> CRUD methods in class \ <Implementation> method existByGrade

--- src/Components/Services/Grades/GradesManager.php
+++ src/Components/Services/Grades/GradesManager.php
@@ -1,27 +1,37 @@
 <?php
 require_once __DIR__ . '/../BaseManager.php';
-require_once __DIR__ . '/../../../utils/validator.php';
 class GradesManager extends BaseManager{
     private Validator $val;
+    const TABLE = 'grade_name';
 
-    public function __construct(GradesRepository $repo, Validator $val){
+    public function __construct(MySQLRepositorie $repo, Validator $val){
         parent::__construct($repo);
         $this->val = $val;
     }
     
     public function create(array $data): array{
-        return parent::create($data);
+        $newGrade = $data['grade_name'];
+        if($this->existByGrade($newGrade)) return existentEntity($newGrade);
+        return parent::createQuery(self::TABLE, $data);
     }
 
     public function read(array $conditions = [], array $cols = ['*']): array{
-        return parent::read($conditions, $cols);
+        return parent::readQuery(self::TABLE, $conditions, $cols);
     }
 
     public function update(array $data, array $conditions): array{
-        return parent::update($data, $conditions);
+        return parent::updateQuery(self::TABLE, $data, $conditions);
     }
 
     public function delete(array $conditions = []): array{
-        return parent::delete($conditions);
+        return parent::deleteQuery(self::TABLE, $conditions);
+    }
+
+    public function existByGrade(string $grade){
+        $conditions = [['column' => '', 'op' => '=', 'val' => $grade, 'bool' => 'AND']];
+        $columns = ['grade_name'];
+        $group = parent::readQuery(self::TABLE, $conditions, $columns);
+        if(!empty($group)) return true;
+        return false;
     }
 }
